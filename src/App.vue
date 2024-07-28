40257a3f5224d9c4c02f86ae2a3f72515d2a870a
Darya <daryaleys@ya.ru>
2024-07-28 14:13:37 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+// const bodyElement = document.body;
+// console.log(bodyElement)
+
+const toggleMode = () => {
+  document.documentElement.classList.toggle('dark');
+}
+
+</script>
+
+<template>
+  <button @click="toggleMode">Toggle Mode</button>
+  <h1>Тест</h1>
+</template>
+
+<style lang="scss" scoped>
+  h1 {
+    color: var(--color-main-text);
+  }
+</style>
